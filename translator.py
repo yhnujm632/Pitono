83b3ba04e7d75f37dca53a44ce5c2adb06baab18
yhnujm632 <167638565+yhnujm632@users.noreply.github.com>
2024-10-22 20:13:48 -0400

    Delete translator.py

--- translator.py
+++
@@ -1,9 +0,0 @@
-import sys
-from googletrans import Translator
-n = len(sys.argv)
-if n == 4:
-   translator = Translator()
-   translation = translator.translate(sys.argv[1], src=sys.argv[2], dest=sys.argv[3])
-   print(translation.text)
-else:
-   print("\033[91mInvalid number of arguments\033[0m")
